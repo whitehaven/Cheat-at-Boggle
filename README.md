d46bce56a3e262c0d0638aa78267e53f4a095aa9
Alex Crist <accrist@gmail.com>
2016-03-13 17:56:15 -0600

    Skeletoned project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Cheat-at-Boggle
+
+This is a boggle solver. I'm not happy with my abilities in Python, so this will be a good exercise.
---
+++ bogglecheat.py
@@ -0,0 +1,22 @@
+class BoggleBoard:
+    def __init__(self, board):
+        self.board = board
+
+    def __repr__(self):
+        repr_string = "BoggleBoard(\n"
+        for lines in self.board:
+            repr_string += str(lines) + "\n"
+        repr_string += ")"
+
+        return repr_string
+
+    def solve_boggle(self, check_dictionary):
+        found_words = []
+        return found_words
+
+
+if __name__ == "__main__":
+    test_dictionary = {'babe'}
+    testBoard = BoggleBoard([['b', 'a'], ['e', 'b']])
+    test_found_words = testBoard.solve_boggle(test_dictionary)
+    print(testBoard)
